fe288c052feb15e19b7ad5a2394826789cc25156
ishtvan-lucor <ishtvan.koloshynsky@gmail.com>
2019-02-24 13:36:01 +0200

    add read_inp() func

--- read_inp.php
+++ read_inp.php
@@ -1,5 +1,34 @@
 <?php
     require_once("global.php");
 
+    function read_inp($input)
+    {
+        global $max_row;
+        global $max_col;
+        global $min_i;
+        global $max_i;
+        global $pizza;
 
-?>
+        if ($input == NULL) {
+            echo "Usage: read_from_file_and_set_gloval(argv[1]);\n";
+            exit(1) ;
+        }
+
+        $file = file($input);
+        $first_line = $file[0];
+        array_shift($file);
+
+        $first_line = explode(" ", $first_line);
+
+        $max_row = $first_line[0];
+        $max_col = $first_line[1];
+        $min_i = $first_line[2];
+        $max_i = $first_line[3];
+
+        print_r($file);
+        foreach ($file  as $key => $value) {
+            trim($value, "\n");
+            $pizza[$key] = str_split(trim($value, "\n"));
+
+        }
+    }
